ca059cacb499ab1039e66b04c09cacb5ea4e83c6
BIOPZ-Puller Vadim <vadim.puller@unibas.ch>
2017-07-26 15:29:54 +0200

    clean for merge

--- K31_prediction.py
+++ K31_prediction.py
@@ -60,20 +60,6 @@ def translate_measures(measure):
     funcnames = ['ambiguous_above', 'hamming_above', 'entropy_above']
     return funcnames[names.index(measure)]
 
-#def ttest_region(func_name, j0jL, cutoff, method, rf = rframe):
-#    CUT = EDI.window_cutoff(data, func_name, region(j0jL), cutoff, rf = rf)
-#    print CUT.ttk.shape
-#    ttk, xxk, jjk = CUT.realdata(Tmin, Tmax,  fcr = fcr, vload_min = vload_min,
-#                                 dilutions_min = dilutions_min)
-#    ttk_est = np.zeros(ttk.shape)
-#    dtdx_t0 = np.zeros((Npat, 2))
-#    for jpat in xrange(Npat):
-#        idx_pat = np.where(jjk == jpat)[0]
-#        idx_data = np.where(jjk != jpat)[0]
-#        ttk_data, dtdx_t0[jpat,:] = EDI.fitmeth_byname(ttk[idx_data], xxk[idx_data], method = method)
-#        ttk_est[idx_pat] = dtdx_t0[jpat,0]*xxk[idx_pat] + dtdx_t0[jpat,1]
-#    return ttk_est, ttk, xxk, jjk, dtdx_t0
-
 
 def loadK31(reg, filepath, fromHIV = False):
     data = {}
@@ -123,8 +109,6 @@ def K31_diversity(data, cutoff, rf = rframe, fcr = 0.5, verbose = False):
     samples = []
     for j, key in enumerate(data.keys()):
         (Tloc, af) = data[key]
-#        if np.sum(af.mask) > 0:
-#            print key, np.mean(np.sum(af.mask, axis =0) >0)
         if np.mean(np.sum(af.mask, axis=0)>0) > fcr:
             if verbose:
                 print 'Omitting {}'.format(key)
@@ -139,64 +123,11 @@ def K31_diversity(data, cutoff, rf = rframe, fcr = 0.5, verbose = False):
         j = key.rfind('_')
         pats.append(key[:j])
         samples.append(key[j+1:])
-#        idx = np.where(np.max(af, axis = 0) <= 1. - cutoff)[0]
-#        DD[j] = np.mean(1. - np.ma.sum(af[:,idx]**2, axis = 0))
     return np.array(TT)/365.25, np.array(DD), pats, samples
-
-def prob_bins(bins, lamb):
-    Pbins = .5*np.abs(np.exp(-np.abs(bins[:-1])/lamb) - np.exp(-np.abs(bins[1:])/lamb))
-    j = np.where((bins[:-1] <0)*(bins[1:]>0))[0]
-    if j:
-        Pbins[j] = .5*(np.exp(bins[:-1][j]/lamb) + np.exp(-bins[1:][j]/lamb))
-    return Pbins
     
-#def K31_ETIvsTI_plot(ax, K31results, label_type, show_legend = True):
-#    (TT, TTest, pats, samples) = K31results
-#    meta = pd.read_csv('./K31_data/patients.csv')
-#    meta_sample = pd.read_csv('./K31_data/sample_timeline_sequenced.csv')
-#    meta_table = pd.read_excel('./K31_data/Suppl table 31 patients true TI 170629.xlsx', skiprows = 2)
-#    if label_type in ['id', 'route', 'subtype']:
-#        pats_unique = list(set(pats))
-#        f = (len(pats_unique)//len(cols)+1)
-#        cc = cols*f
-#        mm = [s for s in marks[:f] for j in xrange(len(cols))]
-#        pairs = [[j for j, p in enumerate(pats) if p ==pat] for pat in pats_unique]
-#        for jp, p in enumerate(pairs):
-#            if label_type == 'id':
-#                lab = pats_unique[jp]
-#            elif label_type == 'route':
-#                lab = meta.at[np.where(meta['id'] == pats_unique[jp])[0][0], 'transmission route']
-#            elif label_type == 'subtype':
-#                lab = meta_table.at[np.where(meta['id'] == pats_unique[jp])[0][0], 'HIV-1 subtyp']
-#                
-#            ax.plot(TT[p], TTest[p], ':' + mm[jp] + cc[jp], markersize = ms, label = lab)
-#        if show_legend:
-#            ax.legend(fontsize = 0.4*fs, loc = 0)
-#            ax.set_title(label_type, fontsize = fs)
-#    elif label_type in ['samples', 'templates', 'dilutions']:
-#        f = (len(pats)//len(cols)+1)
-#        cc = cols*f
-#        mm = [s for s in marks[:f] for j in xrange(len(cols))]
-#        for j, sample in enumerate(samples):
-#            if label_type == 'samples':
-#                lab = sample
-#            elif label_type == 'templates':
-#                lab = meta_sample.at[np.where(meta_sample['id'] == sample)[0][0], 'templates approx']
-#            elif label_type == 'dilutions':
-#                lab = meta_sample.at[np.where(meta_sample['id'] == sample)[0][0], 'dilutions']
-#            ax.plot(TT[j], TTest[j], mm[j] + cc[j], markersize = ms, label = lab)
-#        if show_legend:
-#            ax.legend(fontsize = 0.3*fs, loc = 0, ncol = 3)
-#    else:
-#        ax.plot(TT, TTest, 'ob', markersize = 12, label = 'validation data')
-#    return ax
-
 
 def K31_ETIvsTI_plot(ax, K31results, label_type, show_legend = True):
     (TT, TTest, pats, samples) = K31results
-#    meta = pd.read_csv('./K31_data/patients.csv')
-#    meta_sample = pd.read_csv('./K31_data/sample_timeline_sequenced.csv')
-#    meta_table = pd.read_excel('./K31_data/Suppl table 31 patients true TI 170629.xlsx', skiprows = 2)
     if label_type in ['id', 'route', 'subtype']:
         meta = pd.read_csv('./K31_data/meta_patients.csv')
         pats_unique = list(set(pats))
@@ -214,8 +145,7 @@ def K31_ETIvsTI_plot(ax, K31results, label_type, show_legend = True):
                 
             ax.plot(TT[p], TTest[p], ':' + mm[jp] + cc[jp], markersize = ms, label = lab)
         if show_legend:
-            ax.legend(fontsize = 0.4*fs, loc = 0)
-            ax.set_title(label_type, fontsize = fs)
+            ax.legend(fontsize = 0.6*fs, loc = 0, ncol = 3)
     elif label_type in ['samples', 'templates', 'dilutions']:
         meta = pd.read_csv('./K31_data/meta_samples.csv')
         f = (len(pats)//len(cols)+1)
@@ -230,7 +160,7 @@ def K31_ETIvsTI_plot(ax, K31results, label_type, show_legend = True):
                 lab = meta.at[np.where(meta['id'] == sample)[0][0], 'dilutions']
             ax.plot(TT[j], TTest[j], mm[j] + cc[j], markersize = ms, label = lab)
         if show_legend:
-            ax.legend(fontsize = 0.3*fs, loc = 0, ncol = 3)
+            ax.legend(fontsize = 0.5*fs, loc = 0, ncol = 3)
     else:
         ax.plot(TT, TTest, 'ob', markersize = 12, label = 'validation data')
     return ax
@@ -250,18 +180,6 @@ if __name__=="__main__":
     cutoff1 = 0.002
     bypairs = False
     pairs_legend = True
-    label_type = 'id' #'templates' #'subtype' #'dilutions' #'templates' #'route' #'id'
-#    reg = 'pol'
-    
-#    meta = pd.read_csv('./K31_data/patients.csv', usecols = ['id', 'transmission route'])
-#    meta_sample = pd.read_csv('./K31_data/sample_timeline_sequenced.csv', usecols = ['id', 'patient', 'templates approx', 'dilutions'])
-#    meta_table = pd.read_excel('./K31_data/Suppl table 31 patients true TI 170629.xlsx', skiprows = 2)
-#
-#    meta_sample.to_csv(path_or_buf = './K31_data/meta_samples.csv', index = False)
-#    df = meta.copy()
-#    df['subtype'] = [meta_table.at[np.where(meta['id'] == pat)[0][0], 'HIV-1 subtyp'] for pat in meta['id']]
-#    df.to_csv(path_or_buf = './K31_data/meta_patients.csv', index = False)
-        
         
     for reg in ['gag', 'pol']:
         print reg
@@ -283,49 +201,43 @@ if __name__=="__main__":
         TTmax = np.max(TT)
         jj = np.where(ttk <= TTmax)        
 
-        for label_type in ['id', 'route', 'subtype', 'samples', 'templates', 'dilutions']:
-            fig, ax = plt.subplots(1,1, figsize = (1.3*H, 1.3*H))
+        #ETI vs. TI plot
+        fig, ax = plt.subplots(1,1, figsize = (H, H))
+        ax.plot(ttk, ttk_est, linestyle = '', marker = '^', markersize = 0.8*ms, markerfacecolor = 'gray', label = 'training data')
+        ax = K31_ETIvsTI_plot(ax, (TT, TTest, pats, samples), 'id', show_legend = False)
+        ax.plot(np.sort(ttk), np.sort(ttk), linestyle = '--', color = 'gray')
+        ax.set_xlabel('TI [years]', fontsize = fs)
+        ax.set_ylabel('ETI [years]', fontsize = fs)
+        ax.set_xlim([0, 10.])
+        ax.set_ylim([0, 10.])
+        ax.tick_params(labelsize = .8*fs)
+        fig.tight_layout()
+        plt.savefig(outdir_name + 'K31_{}_ETIvsTI_rf2.pdf'.format(reg))
+        plt.close()
+            
+        
+        #ETI vs. TI plots indicating subtype, transmission route, no of templates and dilutions
+        for label_type in ['route', 'subtype', 'templates', 'dilutions']:
+            fig, ax = plt.subplots(1,1, figsize = (2*H, H))
             ax.plot(ttk, ttk_est, linestyle = '', marker = '^', markersize = 0.8*ms, markerfacecolor = 'gray', label = 'training data')
             ax = K31_ETIvsTI_plot(ax, (TT, TTest, pats, samples), label_type)
             ax.plot(np.sort(ttk), np.sort(ttk), linestyle = '--', color = 'gray')
-            ax.plot(np.sort(ttk), 2.+ np.sort(ttk), linestyle = '--', color = 'gray')
-            ax.plot(np.sort(ttk), -2.+ np.sort(ttk), linestyle = '--', color = 'gray')
             ax.set_xlabel('TI [years]', fontsize = fs)
             ax.set_ylabel('ETI [years]', fontsize = fs)
-            ax.set_xlim([0, 10.])
+            ax.set_xlim([0, 20.])
             ax.set_ylim([0, 10.])
             ax.tick_params(labelsize = .8*fs)
             fig.tight_layout()
-            plt.savefig(outdir_name + 'K31_{}_ETIvsTI_rf2_{}_new.pdf'.format(reg, label_type))
+            plt.savefig(outdir_name + 'K31_{}_ETIvsTI_rf2_{}.pdf'.format(reg, label_type))
             plt.close()
 
 
-#        fig, ax = plt.subplots(1,2, figsize = (2.6*H, 1.3*H))
-#        for j, sample in enumerate(samples):
-#            temp = meta_sample.at[np.where(meta_sample['id'] == sample)[0][0], 'templates approx']
-#            try:
-#                ax[0].semilogx(float(temp), TTest[j] - TT[j], mm[j] + cc[j], markersize = ms, label = sample)
-#                ax[1].semilogx(float(temp), np.abs(TTest[j] - TT[j]), mm[j] + cc[j], markersize = ms, label = sample)
-#            except:
-#                None
-#        ax[0].legend(fontsize = 0.3*fs, loc = 0)
-#        ax[0].set_ylabel('ETI - TI [years]', fontsize = fs)
-#        ax[1].set_ylabel('|ETI - TI| [years]', fontsize = fs)
-#        for j in xrange(2):        
-#            ax[j].set_xlabel('# templates', fontsize = fs)
-#            ax[j].tick_params(labelsize = .8*fs)
-#        fig.tight_layout()
-#        plt.savefig(outdir_name + 'K31_{}_error_vs_templates.pdf'.format(reg))
-#        plt.close()
-
-
+        #errors histogram
         dTT = TTest - TT
         dTTmin = np.min(dTT)//1
         dTTmax = np.max(dTT)//1 + 1.
         TTbins = np.linspace(dTTmin, dTTmax, 2*int(dTTmax - dTTmin) +1)
         fig, ax = plt.subplots(1,1, figsize = (H, H))
-#        ax.hist(ttk_est[jj]-ttk[jj], alpha = 0.5, color = 'gray', label = 'training data')
-#        ax.hist(TTest - TT, alpha = 0.5, color = 'b', label = 'validation data')
         ax.hist(ttk_est-ttk, bins = TTbins, color = 'gray', label = 'training data')
         ax.hist(TTest - TT, bins = TTbins, alpha = 0.5, color = 'b', label = 'validation data')
         ax.legend(fontsize = 0.8*fs, loc = 0)
@@ -335,82 +247,6 @@ if __name__=="__main__":
         plt.savefig(outdir_name + 'K31_{}_hist_rf2.pdf'.format(reg))
         plt.close()
         
-        
-        fig, ax = plt.subplots(1,1, figsize = (H, H))
-        nn, bins = np.histogram(ttk_est-ttk, bins = TTbins)
-        cbins = (bins[:-1] + bins[1:])/2.
-        lamb = np.mean(np.abs(ttk_est-ttk))
-        Pbins = prob_bins(bins, lamb)
-        ax.plot((bins[:-1] + bins[1:])/2., nn, marker = 'd', color = 'gray', label = 'training data')
-        ax.plot(cbins, np.sum(nn)*Pbins, linestyle = '--', color = 'gray')
-#        ax.hist(ttk_est[jj]-ttk[jj], alpha = 0.5, color = 'gray', label = 'training data')
-        nn, bins = np.histogram(TTest-TT, bins = TTbins)
-        cbins = (bins[:-1] + bins[1:])/2.
-        lamb = np.mean(np.abs(TTest-TT))
-        Pbins = prob_bins(bins, lamb)
-        ax.plot(cbins, nn, marker = 'o', color = 'b', label = 'validation data')
-        ax.plot(cbins, np.sum(nn)*Pbins, '--b')
-        ax.legend(fontsize = 0.8*fs, loc = 0)
-        ax.set_xlabel('ETI - TI [years]', fontsize = fs)
-        ax.tick_params(labelsize = .8*fs)
-        fig.tight_layout()
-        plt.savefig(outdir_name + 'K31_{}_hist_rf2_tmp.pdf'.format(reg))
-        plt.close()
-        
-        
-        fig, ax = plt.subplots(1,1, figsize = (2*H, 2*H))
-        ax.plot(ttk, xxk, linestyle = '', marker = '^', markersize = 0.8*ms, markerfacecolor = 'gray', label = 'training data')
-        if bypairs:
-            for jp, p in enumerate(pairs):
-                ax.plot(TT[p], DD[p], ':' + mm[jp] + cc[jp], markersize = 12, label = pats_unique[jp])
-            if pairs_legend:
-                ax.legend(fontsize = 0.4*fs, loc = 0)
-        else:
-            ax.plot(TT, DD, 'ob', markersize = 12, label = 'validation data')
-            ax.legend(loc = 0, fontsize = 0.8*fs)
-        ax.plot(dtdx_t0[0]*np.sort(xxk) + dtdx_t0[1], np.sort(xxk), linestyle = '--', color = 'gray')
-        ax.set_xlabel('TI [years]', fontsize = fs)
-        ax.set_ylabel('diversity', fontsize = fs)
-        ax.tick_params(labelsize = .8*fs)
-        fig.tight_layout()
-        plt.savefig(outdir_name + 'K31_{}_diversity_bypat_rf2.pdf'.format(reg))
-#        plt.savefig(outdir_name + 'K31_{}_diversity_bypat_rf2_N{}.pdf'.format(reg, len(pats_unique)))
-        plt.close()
-        
-        
-        # Outliers        
-        fig, ax = plt.subplots(1,1, figsize = (H,H), sharey = True)
-        dTT = np.abs(TTest - TT)
-        dttk = np.abs(ttk_est-ttk)
-        dTmax = np.max(np.concatenate((dTT, dttk)))//1 + 1
-        Tbins = np.linspace(0., dTmax, 2*int(dTmax) + 1)
-        nn, bins = np.histogram(dTT, Tbins)
-        ax.plot(bins[1:], 1. - np.cumsum(nn)/TT.shape[0])
-        nn, bins = np.histogram(dttk, Tbins)
-        ax.plot(bins[1:], 1. - np.cumsum(nn)/ttk.shape[0])
-               
-        dTT = TTest - TT
-        dTT = dTT[np.where(dTT >=0)]
-        nn, bins = np.histogram(dTT, Tbins)
-        ax.plot(bins[1:], (np.sum(nn) - np.cumsum(nn))/TT.shape[0], '--b')
-        dttk = ttk_est-ttk
-        dttk = dttk[np.where(dttk >=0)]
-        nn, bins = np.histogram(dttk, Tbins)
-        ax.plot(bins[1:],  (np.sum(nn) - np.cumsum(nn))/ttk.shape[0], '--g')
-        
-        dTT = TT - TTest
-        dTT = dTT[np.where(dTT >0)]
-        nn, bins = np.histogram(dTT, Tbins)
-        ax.plot(bins[1:], (np.sum(nn) - np.cumsum(nn))/TT.shape[0], ':b')
-        dttk = ttk-ttk_est
-        dttk = dttk[np.where(dttk >0)]
-        nn, bins = np.histogram(dttk, Tbins)
-        ax.plot(bins[1:], (np.sum(nn) - np.cumsum(nn))/ttk.shape[0], ':g')
-        
-        ax.set_xlabel('|ETI - TI| [years]', fontsize = fs)
-        ax.set_ylabel('fraction')
-#        ax.set_ylim((0.,1.))
-        plt.savefig(outdir_name + 'K31_{}_Nout.pdf'.format(reg))
         
         # diversity by codon position
         fig, ax = plt.subplots(1, 3, figsize = (3*H, 2*H), sharey = True)
@@ -430,6 +266,3 @@ if __name__=="__main__":
         fig.subplots_adjust(wspace = 0.1)
         plt.savefig(outdir_name + 'K31_{}_diversity.pdf'.format(reg))
         plt.close()
-        
-
-        
