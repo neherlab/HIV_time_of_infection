589f60efe4ec66ffc00eabba31de5f10e9099cb9
BIOPZ-Puller Vadim <vadim.puller@unibas.ch>
2017-07-28 16:03:46 +0200

    uncommented lines in EDI_median_regression

--- EDI_median_regression.py
+++ EDI_median_regression.py
@@ -33,55 +33,55 @@ if __name__=="__main__":
         outdir_name + '{}_hamming_trajxt.pdf'.format(gene))
 
 
-#    #Sliding window: absolute error for different ws
-#    wws = [1000, 3000, 5000, 7000]
-#    EDI_plot.plot_sliding_ws(measure, cutoff1, wws,\
-#    outdir_name + '{}_LAD_slidingwindow_abserr.pdf'.format(measure[:-6]))
-#    t1 = time.time()
-#    print t1 - t0
+    #Sliding window: absolute error for different ws
+    wws = [1000, 3000, 5000, 7000]
+    EDI_plot.plot_sliding_ws(measure, cutoff1, wws,\
+    outdir_name + '{}_LAD_slidingwindow_abserr.pdf'.format(measure[:-6]))
+    t1 = time.time()
+    print t1 - t0
 
     #Dependence on cutoff
     measures = ['ambiguous_above', 'hamming_above', 'entropy_above']
     ncut = 50
     cc = [np.linspace(.001, .45, 200), np.linspace(0., .45, ncut), 
           np.linspace(0., .45, ncut)]
-#    for gene in EDI_plot.feas:
-#        EDI_plot.plot_median_new(gene, measures,\
-#        cc, outdir_name + '{}_LAD_'.format(gene))
-#
-#    # accuracy
-#    EDI_plot.plot_tEDI_vs_tDI_bypat(j0jL, measure, cutoff1, \
-#    outdir_name + 'pol_{}_LAD_'.format(measure[:-6]))
-#
-#    #ROC curve
-#    EDI_plot.ROC_curves(measure, j0jL, [0., .25], .5,
-#                        outdir_name + 'pol_')
-#
-#    for tcr in [.9, 1., 1.1]:
-#        EDI_plot.ROC_curves(measure, j0jL, [0., .25], tcr,
-#                            outdir_name + 'pol_ROC_cut_{}.pdf'.format(int(10*tcr)))
-#
-#    #Table of slopes and intercepts
-#    cc = np.linspace(.0, .45, 10)
-#    for meas in measures:
-#        EDI_plot.maketable_slopes(j0jL, meas, cc, ['LAD', 'LAD_slope'],\
-#        outdir_name + 'table_pol_{}.txt'.format(meas), rf = None)
-#        EDI_plot.maketable_slopes(j0jL, meas, cc, ['LAD', 'LAD_slope'],\
-#        outdir_name + 'table_pol_{}_rf.txt'.format(meas))
-#
+    for gene in EDI_plot.feas:
+        EDI_plot.plot_median_new(gene, measures,\
+        cc, outdir_name + '{}_LAD_'.format(gene))
+
+    # accuracy
+    EDI_plot.plot_tEDI_vs_tDI_bypat(j0jL, measure, cutoff1, \
+    outdir_name + 'pol_{}_LAD_'.format(measure[:-6]))
+
+    #ROC curve
+    EDI_plot.ROC_curves(measure, j0jL, [0., .25], .5,
+                        outdir_name + 'pol_')
+
+    for tcr in [.9, 1., 1.1]:
+        EDI_plot.ROC_curves(measure, j0jL, [0., .25], tcr,
+                            outdir_name + 'pol_ROC_cut_{}.pdf'.format(int(10*tcr)))
+
+    #Table of slopes and intercepts
+    cc = np.linspace(.0, .45, 10)
+    for meas in measures:
+        EDI_plot.maketable_slopes(j0jL, meas, cc, ['LAD', 'LAD_slope'],\
+        outdir_name + 'table_pol_{}.txt'.format(meas), rf = None)
+        EDI_plot.maketable_slopes(j0jL, meas, cc, ['LAD', 'LAD_slope'],\
+        outdir_name + 'table_pol_{}_rf.txt'.format(meas))
+
 
     #Additional supplementary figures
     #distribution of slopes and intercepts
     EDI_plot.plot_slope_bootstrap(j0jL, measure, cutoff1,\
     outdir_name + 'slope_bootstrap.pdf')
 
-#    # Pearson correlation vs. cutoff
-#    cc = np.linspace(.01, .4)
-#    EDI_plot.plot_corrcoeff0(j0jL, measures, cc, 
-#                             outdir_name + 'pol_corrcoeff.pdf', rf = None)
-#
-#    EDI_plot.plot_corr_rf(j0jL, measure, cc, 
-#                             outdir_name + 'pol_hamming_corr_rf.pdf')
+    # Pearson correlation vs. cutoff
+    cc = np.linspace(.01, .4)
+    EDI_plot.plot_corrcoeff0(j0jL, measures, cc, 
+                             outdir_name + 'pol_corrcoeff.pdf', rf = None)
+
+    EDI_plot.plot_corr_rf(j0jL, measure, cc, 
+                             outdir_name + 'pol_hamming_corr_rf.pdf')
 
     t1 = time.time()
     print t1 - t0
